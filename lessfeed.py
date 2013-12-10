ed052791991d4395e8b5e7f01edd24d8d2048dab
Nanda Sankaran <nanda.sankaran@gmail.com>
2013-12-10 10:05:44 +0530

    Removed static HTML generation

--- lessfeed.py
+++ lessfeed.py
@@ -3,101 +3,61 @@ import feedparser
 import os
 from urllib.parse import urlparse
 
-#===
-TEMPLATE = """
-<html>
-  <head>
-    <title>News</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <h1>News</h1>
-    {{entries}}
-  </body>
-</html>
-"""
-
-ENTRY_TEMPLATE = """
-  <div class="entry">
-     <span class="date">%(date)s</span> - <a href="%(link)s">%(title)s</a> <span class="domain">(%(domain)s)</span>
-  </div>
-"""
-#===
-
-
-DEFAULT_POLL_SECS = 30 * 60
-
-def generate_html(html_file, entries_file):
-    with open(html_file + '.tmp', 'w') as tmp, open(entries_file) as entries_file:
-        header, footer = TEMPLATE.split('{{entries}}', 1)
-        tmp.write(header)
-        tmp.write(entries_file.read())
-        tmp.write(footer)
-        os.rename(tmp.name, html_file)
-    try:
-        os.link(html_file, os.path.join(os.path.split(html_file)[0], 'index.html'))
-    except FileExistsError:
-        pass
-
-def prepend_entries(entries_file, entries):
-    with open(entries_file + '.tmp', 'w') as tmp, open(entries_file, 'a+') as entries_file:
-        for entry in entries:
-            tmp.write(ENTRY_TEMPLATE %entry)
-        entries_file.seek(0)
-        tmp.write(entries_file.read())
-        os.rename(tmp.name, entries_file.name)
+POLL_INTERVAL = 30 * 60 # seconds
 
 def poll(feeds):
     entries = []
     for feed in feeds.values():
-        print('polling -> %(url)s' %feed)
-        domain = urlparse(feed['url']).netloc
-        parsed_feed = feedparser.parse(feed['url'], etag=feed.get('etag'), modified=feed.get('modified'))
+        print("polling -> %s" %feed['url'])
+        parsed_feed = feedparser.parse(feed['url'], etag=feed['etag'], modified=feed['modified'])
         for entry in parsed_feed.entries:
-            last_polled = feed.get('last_polled')
-            if not last_polled or time.mktime(entry['published_parsed']) > last_polled:
-                e = {}
-                e['date'] = time.strftime("%a,%b%d", entry['published_parsed'])
-                e['title'] = entry['title']
-                e['link'] = entry['link']
-                e['domain'] = domain
-                entries.append(e)
-        feed['etag'] = parsed_feed.get('etag', '')
-        feed['modified'] = time.mktime(parsed_feed.get('modified_parsed'))  if 'modified_parsed' in parsed_feed else ''
-        feed['last_polled'] = time.mktime(time.localtime())
+            published_time = time.mktime(entry['published_parsed'])
+            if feed['last_polled'] is not None and feed['last_polled'] > published_time:
+                continue
+            date = time.strftime("%Y-%m-%d %H:%M:%S", entry['published_parsed'])
+            entries.append({
+                'date': date,
+                'title': entry['title'],
+                'link': entry['link'],
+            })
+        modified_date = parsed_feed.get('modified_parsed')
+        feed['modified'] =  int(time.mktime(modified_date)) if modified_date else None
+        feed['etag'] = parsed_feed.get('etag')
+        feed['last_polled'] = int(time.mktime(time.localtime()))
+    return entries
 
-    if len(entries) > 0:
-        month = time.strftime("%Y-%m", time.localtime())
-        monthly_entries_file = './public/entries-' + month
-        monthly_html_file = './public/' + month + '.html'
-        prepend_entries(monthly_entries_file, entries)
-        generate_html(monthly_html_file, monthly_entries_file)
-    print('fetched %d entries' %len(entries))
-
-
-def run(feedlist_file, tracker_file):
+def run(feedlist_file):
     while 1:
+        tracker_file = './tracker'
+        entries_file = './entries.txt'
         feeds = {}
-        with open(feedlist_file) as feedlist, open(tracker_file, 'a+') as tracker:
-            for line in feedlist:
-                feed_url = line.strip()
-                if feed_url.startswith('#'):
-                    continue
-                if len(feed_url) > 0:
-                    feeds[feed_url] = {'url': feed_url}
-            tracker.seek(0)
-            for line in tracker:
-                parts = line.strip().split(' ')
-                feed = feeds.get(parts[0])
-                if feed is not None:
-                    feed['etag'] =  parts[1] if len(parts) > 1 else None
-                    feed['modified'] = parts[2] if len(parts) > 2 else None
-                    feed['last_polled'] = int(parts[3] if len(parts) > 3 else 0)
-        poll(feeds)
-        with open(tracker_file, 'w') as tracker:
+        with open(feedlist_file) as f:
+            for line in f:
+                feedurl = line.strip()
+                if len(feedurl) > 0:
+                    feeds[feedurl] = {'url': feedurl, 'last_polled': None, 'etag': None, 'modified': None}
+        try:
+            with open(tracker_file, 'r') as t:
+                for line in t:
+                    feedurl, *rest = line.strip().split(' ')
+                    if feedurl in feeds:
+                        feeds[feedurl]['last_polled'] = int(rest[0]) if (len(rest) > 0) else None
+                        feeds[feedurl]['etag'] =        rest[1] if (len(rest) > 1) else None
+                        feeds[feedurl]['modified'] =    int(rest[2]) if (len(rest) > 2) else None
+        except FileNotFoundError:
+            pass
+        entries = poll(feeds)
+        print("fetched %d entries" %len(entries))
+        if len(entries) > 0:
+            with open(entries_file, 'a+') as e:
+                for entry in entries:
+                    e.write("%(date)s\t%(title)s\t%(link)s\n" %entry)
+        with open(tracker_file, 'w') as t:
             for feed in feeds.values():
-                tracker.write("%(url)s %(etag)s %(modified)s %(last_polled)d\n" %feed)
-        time.sleep(DEFAULT_POLL_SECS)
+                feed = dict([k, v if v else ''] for k,v in feed.items())
+                t.write("%(url)s %(last_polled)s %(etag)s %(modified)s\n" %feed)
+        time.sleep(POLL_INTERVAL)
 
-run('./feedlist', './tracker')
+run('./feedlist')
+
 
